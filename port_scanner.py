afd23de07fa4e3c980adea60a7ca390cef26c6fb
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-13 00:14:26 +0000

    2023年02月13日 - 01

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,19 @@
 import socket
 
-def get_open_ports(target, port_range):
+def get_open_ports(target, port_range, bool = False):
     open_ports = []
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
 
+    for port in range(port_range[0], port_range[1]):
+        if sock.connect_ex((target, port)) == 0:
+            open_ports.append(port)
+            sock.close()
+
+    if bool:
+        print('bool == True')
+    else:
+        print('bool == False')
 
     return(open_ports)
