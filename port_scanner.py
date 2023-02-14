c4c3b68f57577aaf48b73ff710f6029cf75038a1
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-14 01:36:52 +0000

    2023-02-14 : 03

--- port_scanner.py
+++ port_scanner.py
@@ -12,57 +12,55 @@ def check_ip_addr(str):
         return False # Hostname
 
 # Main Function
-def get_open_ports(target, port_range, bool = False):
-    open_ports = []
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(5)
-    
-    for port in range(port_range[0], port_range[1] + 1):
-        status = sock.connect_ex((target, port))
-        #print(f'DEBUG : {status}')
-        if status == 0:
-            open_ports.append(port)
-            sock.close()
-    
-    return open_ports
-
 '''
 def get_open_ports(target, port_range, bool = False):
     open_ports = []
     
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(5)
+    for port in range(port_range[0], port_range[1] + 1):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        status = sock.connect_ex((target, port))
+        if status == 0:
+            open_ports.append(port)
+        sock.close()
+    
+    return open_ports
+'''
+#'''
+def get_open_ports(target, port_range, bool = False):
+    open_ports = []
 
-    if check_ip_addr(target):
-        try:
+    try:
+        if check_ip_addr(target):
             host = socket.gethostbyaddr(target)[0]
             addr = target
-        except:
-            result = "Error: Invalid IP address"
-            return result
-    else:
-        try:
+        else:
             host = target
             addr = socket.gethostbyname(target)
-        except socket.gaierror as e:
-            result = "Error: Invalid hostname"
-            return result
+    except socket.gaierror:
+        result = "Error: Invalid IP address"
+        return result
+    except socket.herror:
+        result = "Error: Invalid hostname"
+        return result
     
-    for port in range(port_range[0], port_range[1]):
+    for port in range(port_range[0], port_range[1] + 1):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
         status = sock.connect_ex((host, port))
         if status == 0:
             open_ports.append(port)
-            sock.close()
+        sock.close()
 
-    if not bool:
+    if bool == False:
         return(open_ports)
     else:
         result = ''
         result += f'Open ports for {host} ({addr})\n'
         result += f'PORT     SERVICE\n'
         for i in open_ports:
+            #print(f'DEBUG : {i}')
             name = ports_and_services[i]
-            result += f'{port}       {name}\n'
+            result += f'{i}       {name}\n'
         return result[:-1]
-'''
+#'''
