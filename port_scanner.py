6823a8376c61dcf31523325a8fbb4e8f8f1065ee
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-14 00:47:19 +0000

    2023-02-14 : 02

--- port_scanner.py
+++ port_scanner.py
@@ -17,6 +17,22 @@ def get_open_ports(target, port_range, bool = False):
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(5)
+    
+    for port in range(port_range[0], port_range[1] + 1):
+        status = sock.connect_ex((target, port))
+        #print(f'DEBUG : {status}')
+        if status == 0:
+            open_ports.append(port)
+            sock.close()
+    
+    return open_ports
+
+'''
+def get_open_ports(target, port_range, bool = False):
+    open_ports = []
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
 
     if check_ip_addr(target):
         try:
@@ -49,3 +65,4 @@ def get_open_ports(target, port_range, bool = False):
             name = ports_and_services[i]
             result += f'{port}       {name}\n'
         return result[:-1]
+'''
