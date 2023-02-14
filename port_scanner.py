38ced44d42eae4bc2e97aea8d264ffb49d851cd2
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-14 03:30:24 +0000

    2023-02-14 : 04

--- port_scanner.py
+++ port_scanner.py
@@ -3,29 +3,16 @@ import re
 from common_ports import ports_and_services
 
 # Determine if a string is an IPv4 or a Hostname
-def check_ip_addr(str):
+def check_ip_addr(target):
     reg = re.compile(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-    
-    if reg.search(str):
-        return True # IPv4
+    if reg.search(target):
+        # IPv4
+        return True
     else:
-        return False # Hostname
+        # Host
+        return False
 
 # Main Function
-'''
-def get_open_ports(target, port_range, bool = False):
-    open_ports = []
-    
-    for port in range(port_range[0], port_range[1] + 1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
-        status = sock.connect_ex((target, port))
-        if status == 0:
-            open_ports.append(port)
-        sock.close()
-    
-    return open_ports
-'''
 #'''
 def get_open_ports(target, port_range, bool = False):
     open_ports = []
@@ -38,12 +25,12 @@ def get_open_ports(target, port_range, bool = False):
             host = target
             addr = socket.gethostbyname(target)
     except socket.gaierror:
-        result = "Error: Invalid IP address"
-        return result
-    except socket.herror:
         result = "Error: Invalid hostname"
         return result
-    
+    except socket.herror:
+        result = "Error: Invalid IP address"
+        return result
+
     for port in range(port_range[0], port_range[1] + 1):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(0.5)
@@ -53,14 +40,14 @@ def get_open_ports(target, port_range, bool = False):
         sock.close()
 
     if bool == False:
-        return(open_ports)
+        return open_ports
     else:
         result = ''
         result += f'Open ports for {host} ({addr})\n'
         result += f'PORT     SERVICE\n'
         for i in open_ports:
-            #print(f'DEBUG : {i}')
             name = ports_and_services[i]
-            result += f'{i}       {name}\n'
+            temp = str(i).ljust(9)
+            result += f'{temp}{name}\n'
         return result[:-1]
 #'''
